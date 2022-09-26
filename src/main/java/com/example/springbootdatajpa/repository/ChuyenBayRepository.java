06db3cd79acd4794d7d2fd0d82459caac106f77c
phuong <phuongdenpro@gmail.com>
2022-09-26 14:48:22 +0700

    1

--- src/main/java/com/example/springbootdatajpa/repository/ChuyenBayRepository.java
+++ src/main/java/com/example/springbootdatajpa/repository/ChuyenBayRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface ChuyenBayRepository extends CrudRepository<ChuyenBay, String> {
-    @Query("select s from ChuyenBay s where s.gaDen = :gaDen")
+public interface ChuyenBayRepository extends JpaRepository<ChuyenBay, String> {
+    @Query("select c from ChuyenBay c where c.gaDen = :gaDen")
     List<ChuyenBay> getChuyenBayDen(@Param("gaDen") String gaDen);
 }
