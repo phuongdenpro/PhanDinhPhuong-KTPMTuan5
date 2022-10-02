e5cc8b84a097097c0272cf72f0f59b5f864039e1
phuong <phuongdenpro@gmail.com>
2022-10-02 12:42:01 +0700

    api

--- src/main/java/com/example/springbootdatajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/example/springbootdatajpa/SpringBootDataJpaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
